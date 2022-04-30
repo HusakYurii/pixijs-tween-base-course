99e081557e55901646b818ea14a4354f59ebe009
Husak Yurii <gusak.1881@gmail.com>
2022-04-30 14:48:33 +0200

    added a base class for a tank and its methods

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-import * as PIXI from "./pixi.mjs";
+import { Application, Graphics } from "./pixi.mjs";
 import { assetsMap } from "./assetsMap.js";
+import { Tank } from "./Tank.js";
 
 // Create the application
-const app = new PIXI.Application({
+const app = new Application({
     width: 800,
     height: 800,
     backgroundColor: 0xc2c2c2,
@@ -10,8 +11,20 @@ const app = new PIXI.Application({
 });
 
 const runGame = () => {
-    console.log("LOADED!");
+    const marker = new Graphics();
+    marker.beginFill(0xff0000, 1);
+    marker.drawCircle(0, 0, 5);
+    marker.endFill();
+
+    const tank = new Tank();
+    app.stage.addChild(tank.view);
+    app.stage.addChild(marker);
+
+    app.stage.position.set(800 / 2, 800 / 2);
+
+    window["TANK"] = tank;
+
 };
 
-assetsMap.sprites.forEach((value) => app.loader.add(value));
+assetsMap.sprites.forEach((value) => app.loader.add(value.name, value.url));
 app.loader.load(runGame);
